c2fa561209b975ba3354a1a8d21a7bf8aadb33cb
Protsak Petro <49090552+ProtsakPetro@users.noreply.github.com>
2023-11-09 15:40:15 +0100

    basename

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -7,9 +7,9 @@ import { persistor, store } from './reduxe/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ProtsakPetro/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
